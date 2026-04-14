c2670b3ae11e369282f8a86b27075f82fc5db848
Oracle Cloud Native Environment Authors <noreply@oracle.com>
2026-04-14 13:01:29 +0000

    Disable wasm in Bazel envoy builds
    Build only `//:envoy` and pass `--define=wasm=disabled` so Bazel skips the failing wasm and Python toolchain analysis. This restores `make build` and allows the RPM step to copy the generated `bazel-bin/envoy`.

--- build_istio_proxy.sh
+++ build_istio_proxy.sh
@@ -15,7 +15,8 @@ if [[ $(version $istio_version) -ge $(version "1.18.0") ]]; then
     cpu_count="16"
 fi
 
-BAZEL_BUILD=' --local_cpu_resources='$cpu_count' --copt=-DENVOY_IGNORE_GLIBCXX_USE_CXX11_ABI_ERROR=1 --verbose_failures --copt=-DNDEBUG'
+BAZEL_BUILD=' --local_cpu_resources='$cpu_count' --copt=-DENVOY_IGNORE_GLIBCXX_USE_CXX11_ABI_ERROR=1 --verbose_failures --copt=-DNDEBUG --define=wasm=disabled'
+BAZEL_TARGETS=//:envoy
 if [[ $(version $istio_version) -lt $(version "1.10.0")  \
      ||  $(version $istio_version) -ge $(version "1.15.0") ]]; then
     ENVOY_REPO=--override_repository=envoy="${LOCAL_ENVOY_PROJECT}"
@@ -25,6 +26,7 @@ else
 fi
 BAZEL_BUILD_LOG="/tmp/build.log"
 export BAZEL_BUILD_ARGS
+export BAZEL_TARGETS
 
 if [[ -f ~/.npmrc ]]; then
     cp -f ~/.npmrc /mnt/.npmrc
